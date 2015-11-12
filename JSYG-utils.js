ac206711149b393995437867c97c87d7bbdbf1ab
ybochatay <contact@ybochatay.fr>
2015-11-12 23:58:35 +0100

    bug sur la fonction style2attr

--- JSYG-utils.js
+++ JSYG-utils.js
@@ -284,7 +284,7 @@
             
             if (isSVG && JSYG.svgGraphics.indexOf(this.tagName) == -1) return;
             
-            var style = jThis.getComputedStyle(),
+            var style = getComputedStyle(this),
             defaultStyle = jThis.getDefaultStyle(),
             styleAttr = '',
             name,value,
